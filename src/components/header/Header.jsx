a65dc340afa3abff53df24e6fa8423057a395534
DaMa-sys <danieledelprete.ddp@gmail.com>
2024-06-09 18:59:43 +0200

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -34,7 +34,7 @@ const Header = ({ onScrollToSection,selectRef }) => {
         {onScrollToSection ? (
           <ul>
             <li onClick={() => onScrollToSection("prodotti")}>Prodotti</li>
-            <li onClick={() => onScrollToSection("chiSiamo")}>Chi Siamo</li>
+            <li onClick={() => onScrollToSection("chiSiamo")}>Contatti</li>
           </ul>
         ) : (
           <ul>
